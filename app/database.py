f6cdd5fe29c40e2e6f8001f06154666e2e69c468
Kabirat Akinyemi <akinyemi08140047545@gmail.com>
2026-04-23 11:34:34 +0100

    new update

--- app/database.py
+++ app/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "sqlite:///./profiles.db"
+DATABASE_URL = "sqlite:///./app/profiles.db"
 
 engine = create_engine(
     DATABASE_URL,
